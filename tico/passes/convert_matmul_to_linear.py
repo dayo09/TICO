7eacac55d17cb207b6795bc94aed6240d4c43492
Dayoung Lee <dayoung.lee@samsung.com>
2025-09-18 19:17:50 +0900

    [passes] Add single-batch bmm to convert_lhs_const_mm_to_linear pass
    Let's support single-batch bmm to convert_lhs_const_mm_to_linear pass.
    
    TICO-DCO-1.0-Signed-off-by: Dayoung Lee <dayoung.lee@samsung.com>

--- tico/passes/convert_matmul_to_linear.py
+++ tico/passes/convert_matmul_to_linear.py
@@ -20,11 +20,14 @@ import torch
 from torch._export.utils import is_buffer, is_lifted_tensor_constant, is_param
 from torch.export import ExportedProgram
 
+from tico.serialize.circle_mapping import extract_shape
+
 from tico.utils import logging
 from tico.utils.graph import create_node
 from tico.utils.passes import PassBase, PassResult
 from tico.utils.trace_decorators import trace_graph_diff_on_pass
-from tico.utils.validate_args_kwargs import MatmulArgs
+from tico.utils.utils import set_new_meta_val
+from tico.utils.validate_args_kwargs import BmmArgs, MatmulArgs
 
 
 class Converter:  # type: ignore[empty-body]
@@ -93,6 +96,98 @@ class RhsConstMatmulToLinearConverter(MatmulToLinearConverter):
         return super().convert(exported_program, node)
 
 
+class LhsConstSingleBatchBmmToMmConverter(Converter):
+    """
+    PREPROCESS FOR `LhsConstMatmulToLinearConverter`
+
+    Convert `single-batch lhs-const bmm` to `reshape + matmul`
+
+    lhs(const)  rhs                lhs           rhs
+    |[1,s0,s1]  |[1,s1,s2]          |[1,s0,s1]   |[1,s1,s2]
+    |           |                reshape         reshape
+    |           |                   |[s0,s1]     |[s1,s2]
+     --- bmm  ---         -->        ---- mm ----
+         |                                |[s0,s2]
+         out[1,s0,s2]                     reshape
+                                          |[1,s0,s2]
+                                          out
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def match(self, exported_program, node) -> bool:
+        if not node.target == torch.ops.aten.bmm.default:
+            return False
+
+        bmm_args = BmmArgs(*node.args, **node.kwargs)
+        lhs = bmm_args.input
+        rhs = bmm_args.mat2
+
+        # [Cond1] lhs is constant
+        if not isinstance(lhs, torch.fx.Node):
+            return False
+        if not (
+            is_lifted_tensor_constant(exported_program, lhs)
+            or is_param(exported_program, lhs)
+            or is_buffer(exported_program, lhs)
+        ):
+            return False
+
+        # [Cond2] Both lhs/rhs has batch 1
+        lhs_shape = extract_shape(lhs)
+        rhs_shape = extract_shape(rhs)
+
+        assert len(lhs_shape) == len(
+            rhs_shape
+        ), f"Bmm input's ranks must be the same but got {lhs_shape} and {rhs_shape}"
+
+        if not (lhs_shape[0] == rhs_shape[0] == 1):
+            return False
+
+        return True
+
+    def convert(self, exported_program, node) -> torch.fx.Node:
+        graph_module = exported_program.graph_module
+        graph = graph_module.graph
+
+        bmm_args = BmmArgs(*node.args, **node.kwargs)  # type: ignore[arg-type]
+
+        lhs = bmm_args.input
+        rhs = bmm_args.mat2
+
+        with graph.inserting_before(node):
+            lhs_reshape = create_node(
+                graph,
+                torch.ops.aten.reshape.default,
+                args=(lhs, list(extract_shape(lhs))[1:]),
+            )
+            # NOTE: Call a series of `set_new_meta_val` in order to extract shape of `mm_node` below
+            set_new_meta_val(lhs_reshape)
+            rhs_reshape = create_node(
+                graph,
+                torch.ops.aten.reshape.default,
+                args=(rhs, list(extract_shape(rhs))[1:]),
+            )
+            set_new_meta_val(rhs_reshape)
+            mm_node = create_node(
+                graph,
+                torch.ops.aten.mm.default,
+                args=(lhs_reshape, rhs_reshape),
+            )
+            set_new_meta_val(mm_node)
+            mm_reshape = create_node(
+                graph,
+                torch.ops.aten.reshape.default,
+                args=(mm_node, [1] + list(extract_shape(mm_node))),
+            )
+            set_new_meta_val(mm_reshape)
+
+            node.replace_all_uses_with(mm_reshape, propagate_meta=False)
+
+        return mm_reshape
+
+
 class LhsConstMatmulToLinearConverter(MatmulToLinearConverter):
     def __init__(self):
         super().__init__()
@@ -121,7 +216,7 @@ class LhsConstMatmulToLinearConverter(MatmulToLinearConverter):
 @trace_graph_diff_on_pass
 class ConvertMatmulToLinear(PassBase):
     """
-    This pass converts matmul to linear selectively
+    This pass converts matmul(partially includes single-batch bmm) to linear selectively
 
     How to select between `matmul` and `linear`?
 
@@ -168,6 +263,9 @@ class ConvertMatmulToLinear(PassBase):
         super().__init__()
         self.converters: List[Converter] = []
         if enable_lhs_const:
+            self.converters.append(
+                LhsConstSingleBatchBmmToMmConverter()
+            )  # Preprocess to support single-batched bmm
             self.converters.append(LhsConstMatmulToLinearConverter())
         if enable_rhs_const:
             self.converters.append(RhsConstMatmulToLinearConverter())
